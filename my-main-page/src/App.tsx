cc07cf2bd1e86b2446c2fe07528a9e98dd07e64b
N-2023010\user <newyoung124@naver.com>
2025-06-09 16:00:49 +0900

    sub-projects

--- my-main-page/src/App.tsx
+++ my-main-page/src/App.tsx
@@ -1,13 +1,24 @@
-import './App.css'
+import './App.css';
 
 function App() {
   return (
     <div style={{ textAlign: 'center', marginTop: '20vh' }}>
       <h1>Welcome to YoungLab</h1>
       <p>철학과 반복을 사랑하는 영쥐의 YoungLab 입니다 🪷</p>
-      <button> Hello </button>
+
+      <div style={{ marginTop: '2rem', display: 'flex', gap: '1rem', justifyContent: 'center' }}>
+        <a href="/calgal">
+          <button>📅 CalGal</button>
+        </a>
+        <a href="/habitgarden">
+          <button>🌱 HabitGarden</button>
+        </a>
+        <a href="/readmeter">
+          <button>📚 ReadMeter</button>
+        </a>
+      </div>
     </div>
   );
 }
 
-export default App
+export default App;
