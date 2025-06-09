df954e34bb2cd2d557ae94c4a1ab92e186f8e0e9
N-2023010\user <newyoung124@naver.com>
2025-06-09 15:03:46 +0900

    first commit

---
+++ my-main-page/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '20vh' }}>
+      <h1>Welcome to YoungLab</h1>
+      <p>철학과 반복을 사랑하는 영쥐의 YoungLab 입니다 🪷</p>
+      <button> Hello </button>
+    </div>
+  );
+}
+
+export default App
